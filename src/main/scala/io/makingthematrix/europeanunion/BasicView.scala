164b2667207153c00b6e7e000310ba7013793098
Maciej Gorywoda <makingthematrix@gmail.com>
2021-01-16 21:36:45 +0100

    Refactoring to EU + Scotland

--- src/main/scala/io/makingthematrix/europeanunion/BasicView.scala
+++ src/main/scala/io/makingthematrix/europeanunion/BasicView.scala
@@ -4,12 +4,12 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * * Redistributions of source code must retain the above copyright
+ *   * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright
+ *   * Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * * Neither the name of Gluon, any associated website, nor the
+ *   * Neither the name of Gluon, any associated website, nor the
  * names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
  *
@@ -23,44 +23,51 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Based on https://github.com/gluonhq/gluon-samples/tree/master/fiftystates
  */
-package io.makingthematrix.fiftystates
+
+package io.makingthematrix.europeanunion
 
 import com.gluonhq.charm.glisten.control._
 import com.gluonhq.charm.glisten.mvc.View
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon
-import io.makingthematrix.fiftystates.model.Density.Density
-import io.makingthematrix.fiftystates.model.{Density, USState, USStates}
+import io.makingthematrix.europeanunion.model.Density.Density
+import io.makingthematrix.europeanunion.model.{Country, Density}
+import javafx.collections.FXCollections
 import javafx.collections.transformation.FilteredList
 import javafx.event.{ActionEvent, EventHandler}
 import javafx.scene.control.{MenuItem, RadioMenuItem, ToggleGroup}
+import javafx.scene.image.Image
 
 import java.util.function.Predicate
 import scala.jdk.CollectionConverters._
 import scala.jdk.FunctionConverters._
 
+import model.Country._
+
 object BasicView {
   def apply(): BasicView = {
-    val filteredList = new FilteredList(USStates.statesList, (_: USState) => true)
-    val charmListView = createCharmListView(filteredList)
-    returning(new BasicView(filteredList, charmListView)) { _.setCenter(charmListView) }
+    val filteredList = new FilteredList(FXCollections.observableArrayList((countriesList ++ Seq(Scotland)).asJavaCollection), (_: Country) => true)
+    val listView = createListView(filteredList)
+    returning(new BasicView(filteredList, listView)) { _.setCenter(listView) }
   }
 
-  private def createCharmListView(filteredList: FilteredList[USState]) =
-    returning(new CharmListView[USState, Density](filteredList)) { view =>
-      view.setCellFactory((_: CharmListView[USState, Density]) => USStateCell())
-      view.setHeadersFunction((Density.stateToDensity _).asJava)
-      view.setHeaderCellFactory((_: CharmListView[USState, Density])  => new CharmListCell[USState]() {
+  private def createListView(filteredList: FilteredList[Country]) =
+    returning(new CharmListView[Country, Density](filteredList)) { view =>
+      view.setCellFactory((_: CharmListView[Country, Density]) => CountryCell())
+      view.setHeadersFunction((Density.getDensity _).asJava)
+      view.setHeaderCellFactory((_: CharmListView[Country, Density])  => new CharmListCell[Country]() {
         private final val tile = new ListTile()
-        private final val usFlag = new Avatar(16)
 
-        tile.setPrimaryGraphic(usFlag)
-
-        override def updateItem(item: USState, empty: Boolean) {
+        override def updateItem(item: Country, empty: Boolean) {
           super.updateItem(item, empty)
           if (!empty) {
-            USStates.getImage(USStates.usState.flag).foreach(flag => usFlag.setImage(flag))
-            tile.textProperty.setAll(USStates.usState.toString)
+            val flag = new Image(getClass.getResourceAsStream("100px-Flag_of_Europe.svg.png"))
+            val euFlag = new Avatar(23, flag)
+            tile.setPrimaryGraphic(euFlag)
+
+            tile.textProperty.setAll(EuropeanUnion.toString)
             // tile.setWrapText(true) causes the header to expand and cover the whole screen
             // TODO: Find out how to handle it. CSS? SceneBuilder?
             setGraphic(tile)
@@ -70,21 +77,21 @@ object BasicView {
     }
 }
 
-class BasicView(filteredList: FilteredList[USState], charmListView: CharmListView[USState, Density]) extends View {
+final class BasicView(filteredList: FilteredList[Country], listView: CharmListView[Country, Density]) extends View {
   private var ascending = true
 
   override def updateAppBar(appBar: AppBar): Unit = {
-    appBar.setNavIcon(MaterialDesignIcon.STAR.button)
-    appBar.setTitleText("50 States")
+    appBar.setNavIcon(MaterialDesignIcon.STAR.graphic)
+    appBar.setTitleText("European Union")
 
     appBar.getActionItems.add(MaterialDesignIcon.SORT.button({ _ =>
       if (ascending) {
-        charmListView.setHeaderComparator((d1: Density, d2: Density) => d1.initial - d2.initial)
-        charmListView.setComparator((s1: USState, s2: USState) => (s1.density - s2.density).toInt)
+        listView.setHeaderComparator((d1: Density, d2: Density) => d1.initial - d2.initial)
+        listView.setComparator((s1: Country, s2: Country) => (s1.density - s2.density).toInt)
         ascending = false
       } else {
-        charmListView.setHeaderComparator((d1: Density, d2: Density) => d2.initial - d1.initial)
-        charmListView.setComparator((s1: USState, s2: USState) => (s2.density - s1.density).toInt)
+        listView.setHeaderComparator((d1: Density, d2: Density) => d2.initial - d1.initial)
+        listView.setComparator((s1: Country, s2: Country) => (s2.density - s1.density).toInt)
         ascending = true
       }
     }))
@@ -92,8 +99,8 @@ class BasicView(filteredList: FilteredList[USState], charmListView: CharmListVie
     appBar.getMenuItems.setAll(buildFilterMenu.asJavaCollection)
   }
 
-  private def getStatePredicate(population: Double): Predicate[USState] =
-    (state: USState) => state.population >= population * 1_000_000
+  private def getStatePredicate(population: Double): Predicate[Country] =
+    (state: Country) => state.population >= population * 1_000_000
 
   private def buildFilterMenu = {
     val menuActionHandler: EventHandler[ActionEvent] = { e: ActionEvent =>
---
+++ src/main/scala/io/makingthematrix/europeanunion/package.scala
@@ -0,0 +1,23 @@
+package io.makingthematrix
+
+import javafx.beans.value.{ChangeListener, ObservableValue}
+
+import java.util.function.Consumer
+import scala.jdk.FunctionWrappers.AsJavaConsumer
+
+package object europeanunion {
+
+  @inline def returning[T](t: T)(body: T => Any): T = {
+    body(t)
+    t
+  }
+
+  final class ChangeListener4S[T](onNewValue: Consumer[T]) extends ChangeListener[T] {
+    override def changed(observable: ObservableValue[_ <: T], oldValue: T, newValue: T): Unit = onNewValue.accept(newValue)
+  }
+
+  object ChangeListener4S {
+    def apply[T](onNewValue: T => Unit): ChangeListener[T] = new ChangeListener4S[T](AsJavaConsumer(onNewValue))
+  }
+
+}
