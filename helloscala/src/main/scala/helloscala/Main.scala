8c75b383093cec5e5126285ad85624c9bb71516e
Maciej Gorywoda <makingthematrix@gmail.com>
2021-08-09 17:38:46 +0200

    screenshot of helloscala

--- helloscala/src/main/scala/helloscala/Main.scala
+++ helloscala/src/main/scala/helloscala/Main.scala
@@ -22,7 +22,7 @@ object Main {
 
 final class Main extends MobileApplication {
   override def init(): Unit = addViewFactory(MobileApplication.HOME_VIEW, () => {
-    val imageView = new ImageView(new Image(this.getClass.getResourceAsStream("scalalogo.png"))).tap { view =>
+    val imageView = new ImageView(new Image(this.getClass.getResourceAsStream("scala_logo.png"))).tap { view =>
       view.setFitHeight(200)
       view.setPreserveRatio(true)
     }
@@ -34,7 +34,7 @@ final class Main extends MobileApplication {
   })
 
   override def postInit(scene: Scene): Unit = {
-    Swatch.LIGHT_GREEN.assignTo(scene)
+    Swatch.RED.assignTo(scene)
     scene.getStylesheets.add(this.getClass.getResource("styles.css").toExternalForm)
     if (Platform.isDesktop) {
       import scala.jdk.FunctionConverters._
