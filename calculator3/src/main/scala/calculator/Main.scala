3a7c5e0aa3f496f08705994d79a5dd1ccb92f998
Maciej Gorywoda <makingthematrix@gmail.com>
2022-01-17 17:50:41 +0100

    update feom MobileApplication to javafx Application

--- calculator3/src/main/scala/calculator/Main.scala
+++ calculator3/src/main/scala/calculator/Main.scala
@@ -2,13 +2,13 @@ package calculator
 
 import com.gluonhq.attach.display.DisplayService
 import com.gluonhq.attach.util.Platform
-import com.gluonhq.charm.glisten.application.MobileApplication
 import com.gluonhq.charm.glisten.mvc.View
 import com.gluonhq.charm.glisten.visual.Swatch
 import javafx.fxml.FXMLLoader
 import javafx.geometry.Dimension2D
 import javafx.scene.Scene
 import javafx.scene.layout.BorderPane
+import javafx.stage.Stage
 
 import scala.jdk.FunctionConverters.enrichAsJavaFunction
 
@@ -18,22 +18,25 @@ object Main:
   val DEFAULT_WIDTH: Int = 550
   val DEFAULT_HEIGHT: Int = 650
 
-final class Main extends MobileApplication:
+final class Main extends javafx.application.Application:
   import Main._
 
-  override def init(): Unit =
-    val root = FXMLLoader.load[BorderPane](classOf[Main].getResource("main.fxml"))
-    addViewFactory(MobileApplication.HOME_VIEW, () => new View(root))
+  private def dimensions =
+    if Platform.isDesktop then
+      new Dimension2D(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+    else
+      DisplayService.create
+        .map(((ds: DisplayService) => ds.getDefaultDimensions).asJava)
+        .orElse(new Dimension2D(DEFAULT_WIDTH, DEFAULT_HEIGHT))
 
-  override def postInit(scene: Scene): Unit =
+
+  override def start(stage: Stage): Unit =
+    val dim = dimensions
+    val scene = new Scene(FXMLLoader.load[BorderPane](classOf[Main].getResource("main.fxml")), dim.getWidth, dim.getHeight)
     Swatch.AMBER.assignTo(scene)
-    scene.getStylesheets.add(classOf[Main].getResource("styles.css").toExternalForm)
-    val dim =
-      if (Platform.isDesktop) then
-        new Dimension2D(DEFAULT_WIDTH, DEFAULT_HEIGHT)
-      else
-        DisplayService.create
-          .map(((ds: DisplayService) => ds.getDefaultDimensions).asJava)
-          .orElse(new Dimension2D(DEFAULT_WIDTH, DEFAULT_HEIGHT))
-    scene.getWindow.setWidth(dim.getWidth)
-    scene.getWindow.setHeight(dim.getHeight)
+    Option(classOf[Main].getResource("styles.css")).foreach { url =>
+      scene.getStylesheets.add(url.toExternalForm)
+    }
+    stage.setScene(scene)
+    stage.show()
+
