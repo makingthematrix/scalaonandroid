1c89b1f51e9bdd3f137bd474d779ded9b6904e65
Maciej Gorywoda <makingthematrix@protonmail.com>
2023-04-19 14:22:21 +0200

    helloscala example updated to Scala 3.3

--- helloscala/src/main/scala/helloscala/Main.scala
+++ helloscala/src/main/scala/helloscala/Main.scala
@@ -2,7 +2,8 @@ package helloscala
 
 import com.gluonhq.attach.display.DisplayService
 import com.gluonhq.attach.util.Platform
-import com.gluonhq.charm.glisten.application.MobileApplication
+import com.gluonhq.charm.glisten.application.AppManager
+import com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW
 import com.gluonhq.charm.glisten.control.{AppBar, FloatingActionButton}
 import com.gluonhq.charm.glisten.mvc.View
 import com.gluonhq.charm.glisten.visual.{MaterialDesignIcon, Swatch}
@@ -13,37 +14,45 @@ import javafx.scene.control.Label
 import javafx.scene.image.{Image, ImageView}
 import javafx.scene.layout.VBox
 import javafx.application.Application
+import javafx.stage.Stage
 
+import scala.jdk.FunctionConverters.enrichAsJavaFunction
 import scala.util.chaining.scalaUtilChainingOps
 
-object Main {
+object Main:
   def main(args: Array[String]): Unit = Application.launch(classOf[Main], args: _*)
-}
 
-final class Main extends MobileApplication {
-  override def init(): Unit = addViewFactory(MobileApplication.HOME_VIEW, () => {
+  val DEFAULT_WIDTH: Int = 640
+  val DEFAULT_HEIGHT: Int = 480
+
+  lazy val dimensions: Dimension2D =
+    if Platform.isDesktop then
+      new Dimension2D(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+    else
+      DisplayService.create
+        .map(((ds: DisplayService) => ds.getDefaultDimensions).asJava)
+        .orElse(new Dimension2D(DEFAULT_WIDTH, DEFAULT_HEIGHT))
+
+final class Main extends Application:
+  import Main._
+
+  private val appManager = AppManager.initialize(postInit)
+
+  override def init(): Unit =
     val imageView = new ImageView(new Image(this.getClass.getResourceAsStream("scala_logo.png"))).tap { view =>
       view.setFitHeight(200)
       view.setPreserveRatio(true)
     }
-    new View(new VBox(20, imageView, new Label("Hello, Scala!")).tap { _.setAlignment(Pos.CENTER) }) {
-      override protected def updateAppBar(appBar: AppBar): Unit = appBar.setTitleText("Gluon Mobile + Scala 2.13")
+    val root = new View(new VBox(20, imageView, new Label("Hello, Scala!")).tap { _.setAlignment(Pos.CENTER) }) {
+      override protected def updateAppBar(appBar: AppBar): Unit = appBar.setTitleText("Gluon Mobile + Scala 3.3")
     }.tap { view =>
       new FloatingActionButton(MaterialDesignIcon.SEARCH.text, (_: ActionEvent) => println("log something from Scala")).tap { _.showOn(view) }
     }
-  })
+    appManager.addViewFactory(HOME_VIEW, () => root)
 
-  override def postInit(scene: Scene): Unit = {
-    Swatch.RED.assignTo(scene)
-    scene.getStylesheets.add(this.getClass.getResource("styles.css").toExternalForm)
-    if (Platform.isDesktop) {
-      import scala.jdk.FunctionConverters._
-      scene.getWindow.tap { window =>
-        val defaultDimensions = (ds: DisplayService) => ds.getDefaultDimensions
-        val dimension2D = DisplayService.create.map(defaultDimensions.asJava).orElse(new Dimension2D(640, 480))
-        window.setWidth(dimension2D.getWidth)
-        window.setHeight(dimension2D.getHeight)
-      }
-    }
-  }
-}
+  override def start(stage: Stage): Unit = appManager.start(stage)
+
+  private def postInit(scene: Scene): Unit =
+    Swatch.AMBER.assignTo(scene)
+    scene.getWindow.setWidth(dimensions.getWidth)
+    scene.getWindow.setHeight(dimensions.getHeight)
